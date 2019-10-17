6f301bdd89e04f7e511ba79334a94951a9f809bb
Echo Nolan <echo@echonolan.net>
2019-10-17 13:14:22 -0700

    Add dependency on Jemalloc

--- coda.rb
+++ coda.rb
@@ -4,12 +4,13 @@ class Coda < Formula
   url "https://github.com/CodaProtocol/coda/releases/download/0.0.9-beta4/homebrew-coda.tar.gz"
   sha256 "5aee00bec49b0f823964b4a14abbd722ac53335a346ccbc74fafe30456f02d2b"
   revision 15
-
-  depends_on "openssl"
-  depends_on "libsodium"
+  
   depends_on "gmp"
-  depends_on "libomp"
+  depends_on "jemalloc"
   depends_on "libffi"
+  depends_on "libomp"
+  depends_on "libsodium"
+  depends_on "openssl"
   depends_on "miniupnpc" => :recommended
 
   bottle :unneeded
